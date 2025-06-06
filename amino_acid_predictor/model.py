a08a9a9272a47b6244b840ca9efea53514284984
MbuviM <mwendembuvi10@gmail.com>
2025-06-06 18:33:37 +0300

    model predictor

---
+++ amino_acid_predictor/model.py
@@ -0,0 +1,23 @@
+amino_acid_data = {
+    "Histidine": "CAU, CAC", 
+    "Arginine": "CGU, CGC, CGA, CGG, AGA, AGG", 
+    "Lysine": "AAA, AAG","Isoleucine": "AUU, AUC, AUA", 
+    "Aspartic Acid": "GAU, GAC",
+    "Glumatic Acid":"GAA, GAG",
+    "Threonine": "ACU, ACC, ACA, ACG",
+    "Methionine": "AUG", 
+    "Glytamine": "CAA, CAG",
+    "Phenylanine": "UUU, UUC",
+    "Leucine": "CUU, CUC, CUA, CUG, UUA, UUG",
+    "Tryptophan": "UGG",
+    "Alanine": "GCU, GCC, GCA, GCG",
+    "Tyrosine": "UAU, UAC",
+    "Cysteine": "UGU, UGC",
+    "Asparagine": "AAA, AAC",
+    "Valina": "GUU, GUC, GUA, GUG",
+    "Glycine": "GGU, GGC, GGA, GGG",
+    "Serine": "AGU, AGC, UCU, UCC, UCA, UCG",
+    "Proline": "CCU, CCC, CCA, CCG"
+}
+
+print(amino_acid_data.keys())
